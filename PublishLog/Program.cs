4540d9ec3e57323c02bea188d1e88f3be9ca0e1f
yilmazertan <ylmz.ertan@gmail.com>
2024-08-27 15:17:41 +0300

    first commit

---
+++ PublishLog/Program.cs
@@ -0,0 +1,26 @@
+﻿//Bu rabbitmq örneği fanout exchane için tasarladı.Loglar exchange atılır ve clientlar bu exchane bağlanıp aynı anda mesajlara erişebilir
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.HostName = "localhost";
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+channel.ExchangeDeclare(exchange:"logs",ExchangeType.Fanout); //fanout kendisine bağlı ne kadar subscriber varsa hepsine mesajı iletir
+
+var message = GetMessage(args);
+var body=Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange:"logs", routingKey:string.Empty, basicProperties:null , body:body);
+
+Console.WriteLine("Mesaj gönderildi : " + message);
+
+
+
+static string GetMessage(string[]args){
+ return args.Length>0? string.Join(" ",args) : "Info : hello world";
+
+}
+
---
+++ ReceiveLog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+factory.HostName = "localhost";
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+var queueName = channel.QueueDeclare().QueueName; //rastgele bir queue name oluştur
+channel.QueueBind(queue: queueName,exchange:"logs",routingKey:string.Empty); //queue logs adında exchange bind et
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received +=(model,ea)=>{
+
+var body = ea.Body.ToArray();
+var message = Encoding.UTF8.GetString(body);
+Console.WriteLine("Okunan Mesaj : " + message);
+
+};
+
+ channel.BasicConsume(queue:queueName,autoAck:false,consumer:consumer);
+
+ Console.ReadKey();
